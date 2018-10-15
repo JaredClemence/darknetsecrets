b9a3f5128c01ef066c5c31fd986905e8740e87e6
Jared R Clemence <jaredclemence@pediatricheartcenter.org>
2018-10-15 10:32:56 -0700

    - Test that site loads both with and without a referrer.

---
+++ tests/Unit/T00001Test.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Campaign\Referrer;
+
+class T00001Test extends TestCase
+{
+    use RefreshDatabase;
+    
+    /**
+     * @dataProvider provider
+     */
+    public function testPageLoad( $url )
+    {
+        $offerHeading = "Free training about Internet privacy";
+        $registrationButton = "<button type=\"submit\" class=\"btn btn-primary\">Register</button>";
+        $termsAndConditions = "Terms & Conditions";
+        
+        $response = $this->get($url);
+        
+        $response->assertSeeText($offerHeading);
+        $response->assertSee($registrationButton);
+        $response->assertSeeText($termsAndConditions);
+    }
+    public function provider(){
+        parent::setUp();
+        $endpoint = 'registration.new';
+        $ref_id = 1; //factory( Referrer::class )->create();
+        $urlNoReferrer = [ route($endpoint) ];
+        $urlWithReferrer = [ route($endpoint, compact('ref_id')) ];
+        return compact('urlNoReferrer','urlWithReferrer');
+    }
+}
