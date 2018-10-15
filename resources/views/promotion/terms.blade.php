3cb9e7bff5ab4ee7cdf3feb09b861a88d916474d
Jared R Clemence <jaredclemence@pediatricheartcenter.org>
2018-10-15 09:27:16 -0700

    - Updates to Privacy policy.

--- resources/views/promotion/terms.blade.php
+++ resources/views/promotion/terms.blade.php
@@ -7,11 +7,11 @@
             <h2>Definitions</h2>
             <dl>
                 <dt>contest:</dt><dd>a promotional offer that has the form of a 
-                unilateral contract that requires referrals. Contests may be take the 
-                form of a "sweepstakes" (entirely luck-based) or a "contest of merit" (based on 
-                a skill or merit that the participant can manipulate to change odds).
-                A contest is not a lottery--this site will not accept money in exchange 
-                for chances to win.
+                    unilateral contract that requires referrals. Contests may be take the 
+                    form of a "sweepstakes" (entirely luck-based) or a "contest of merit" (based on 
+                    a skill or merit that the participant can manipulate to change odds).
+                    A contest is not a lottery--this site will not accept money in exchange 
+                    for chances to win.
                 </dd>
                 <dt>paying customer:</dt><dd> a verified registrant who visits 
                     a page that is only made accessible to a person who has 
@@ -20,118 +20,142 @@
                 <dt>registered visitor:</dt><dd> any visitor who subsequently 
                     accepts the terms of an offer by entering an email address 
                     into the designated form and submitting the form. </dd>
-            <dt>site:</dt><dd> All pages linked and connected to this 
-                promotional system within the same domain as the domain 
-                on which the offer is presented.</dd>
-            <dt>unverified registrant:</dt><dd> see registered visitor.</dd>
-            <dt>user:</dt><dd> any person who loads and views a page from 
-                this site.</dd>
-            <dt>verified registrant:</dt><dd> any registered visitor who 
-                verifies the accuracy of an email address by clicking a 
-                specially constructed link in an email sent to the provided 
-                address.</dd>
-            <dt>visitor:</dt><dd> any person who loads and views a page 
-                containing a promotional offer.</dd>
+                <dt>site:</dt><dd> All pages linked and connected to this 
+                    promotional system within the same domain as the domain 
+                    on which the offer is presented.</dd>
+                <dt>unverified registrant:</dt><dd> see registered visitor.</dd>
+                <dt>user:</dt><dd> any person who loads and views a page from 
+                    this site.</dd>
+                <dt>verified registrant:</dt><dd> any registered visitor who 
+                    verifies the accuracy of an email address by clicking a 
+                    specially constructed link in an email sent to the provided 
+                    address.</dd>
+                <dt>visitor:</dt><dd> any person who loads and views a page 
+                    containing a promotional offer.</dd>
             </dl>
-<h2>Privacy</h2>
-<p>The purpose of the site is to acquire information on people interested in 
-    participating in the offered program. In performing this duty, the site 
-    will collect email addresses and use tools provided by Facebook and Google 
-    to identify the intended persons. Information will not be used outside of 
-    these purposes or shared with 3rd parties.</p>
-<h2>Prizes and Contests</h2>
-<p>To induce referrals, the site may offer items or services of value 
-    (collectively referred to as a "prize") in exchange for action on the part 
-    of the registered visitor. Each offer, the activity required to accept the 
-    offer, and the associated prize are called "contests" within this section.
-</p>
-<p>A visitor must register separately for each contest. The site will determine 
-    if the visitor is eligible for registration based on the contest rules and 
-    objectives. When admitted to a contest, the visitor becomes a registered 
-    visitor and must click a link emailed to the registered email address to 
-    become a verified registrant. Only verified registrants are eligible to 
-    receive prizes. It is possible for a visitor to be a verified registrant 
-    in one contest and merely a visitor in a separate contest.</p>
-<p>Contests will induce referrals of either additional contest registrants (to 
-    build an email list) or paying customers (to generate profit). Given these 
-    goals, within any one contest, a referral is valid only if the referred 
-    person does the following:</p>
-<ul>
-    <li>Visit the contest offer page without being previously registered for 
-        the same contest;</li>
-    <li>Register during a visit that begins with the referral clicking a link 
-        provided by the referrer;</li>
-    <li>Subsequently, becoming a verified registrant in the same contest; and
-    </li>
-    <li>Performing any required action stated in the offer terms (for example, 
-        making a purchase)</li>
-</ul>
-<p>A contest may allow a verified registrant to claim email addresses before 
-    making a referral for purchase. When the site makes such a process 
-    available, the site will credit verified registrants for purchases made after 
-    the email address is claimed and before it is claimed by another registrant.
-    Two registrants may not claim responsibility for a single purchase.
-</p>
-<p>
-Offers of prizes are limited. They are not binding on the site until the 
-contest reaches a specified volume of activity. Unless stated in the offer, 
-the limit shall be set as follows:</p>
-<ul>
-    <li>An offer intended to increase referrals of verified registrants become 
-        binding upon the site's owner when the number of registrations exceeds 
-        10,000.</li>
-    <li>An offer intended to increase referrals of paying customers will become 
-        binding when net operating income exceeds two times the total value of 
-        the prizes offered.</li>
-</ul>
-<p>A contest offer page may be disabled at any time. A contest will be 
-    terminated 3 months after the offer page is disabled to allow time for any 
-    additional purchases that verified registrants are actively referring in 
-    pursuit of a prize.</p>
-<p>By participating in a contest for a prize, the registered visitor agrees to 
-    receive occasional emails from the site or the site owner. Each email will 
-    provide a link to unsubscribe. Unsubscribing from the email list removes a 
-    registered user from the contest. This notice will be repeated on the 
-    unsubscribe page.</p>
-<h2>Paying Customers</h2>
-<p>A person who pays for services only becomes a paying customer when the 
-    profile of a person who has paid money is successfully linked to the 
-    verified registrant or a claimed email address provided by a verified 
-    registrant.</p>
-<p>The site will manage and maintain a list of purchases, purchase amount and 
-    estimated purchase profit. For this purpose, the site owner will upload or 
-    enter the data that is provided by payment processors. The site will 
-    compare email addresses in the purchase data to registered email addresses 
-    to identified qualified purchases. Qualified purchases will be credited 
-    to the verified registrant who either referred the customer or claimed the 
-    email address as a potential referral. </p>
-<p>The site owner will perform an update of the payments received and 
-    associated email addresses covering the life of the program before 
-    prizes linked to value provided by paying customers are awarded. The update 
-    shall be considered current and up-to-date if the data used is obtained 
-    within 24 hours of the selection, or no additional sales have been made 
-    between the time the last list was uploaded and the moment of the decision.</p>
-<h2>Lotteries</h2>
-<p>This site does not perform or condone the use of lotteries. A lottery is defined 
-    as a game of chance that requires entrants to pay money to improve or have a chance to win.
-    One example of a lottery would be a "gift" of "chances to win" for purchases made; 
-    even though the "chance to win" is named a gift in this hypothetical, the chance to 
-    win is only obtained through providing a purchase, and this would constitute a lottery.</p>
-<p>This site does award chances to win in exchange for referrals of 'paying customers.' This is not 
-    a lottery. The person who receives the chance to win provides a referral only. It does not 
-    cost the registrant any money to obtain the chance, and therefore this is not a lottery but a 
-    "contest of merit."</p>
-<p>For all contests of merit offered by this site, a person has the option of registering for 
-    a unique referral url, and that option will be made available at no cost. Please see the 
-    landing page for the 'contest of merit' registration or contact the site owner for details.</p>
-<h2>Updates to the Policy</h2>
-<p>This policy may be updated at any time. The updated policy becomes active 
-    seven days after notice is transmitted to the registered visitors and takes 
-    immediate effect for any new visitors.</p>
-<p>Offers that have not been accepted completely prior to the effective date 
-    of a new policy are revoked and reissued by the new policy terms. Actions 
-    that were taken under the previous policy will count toward consideration 
-    of the new offer under the new policy.</p>
+            <h2>Privacy</h2>
+            <p>This site collects and stores email addresses from persons interested in offered services. 
+                Partner sites may collect other information from cookies stored in your 
+                browser that this site does not control. Neither can this site access any of 
+                that information that partner sites may have on you.
+            <h2>Prizes and Contests</h2>
+            <p>
+                The site may offer items or services of value (collectively referred to as a "prize") to induce referrals.
+                Unless otherwise specified, a person enters the contest by referring visitors 
+                to the site who then register for future services (free or paid).
+            </p>
+            <p>
+                The consideration in this arrangement shall not require the registrant to pay fees.
+                The site understands that collecting money in exchange for entries constitutes a lottery, and 
+                this site DOES NOT conduct lotteries.
+            </p>
+            <p>
+                The site may run multiple contests simultaneously. The site runs varied contests 
+                to test a variety of offers in pursuit of optimizing performance and increase 
+                unique registrations. 
+            </p>
+            <p>
+                Visitors may register for multiple contests with the same email address. The site 
+                will not provide landing page links on request, visitors must find the contest 
+                landing page via a paid advertisement or friend's referral.
+            </p>
+            <p>
+                Visitors obtain entries to contests by registering valid email addresses. 
+                Before the site provides an entry into a contest, the registered address 
+                must be validated by the registrant. The site will send a unique url 
+                to the provided email address that must be loaded in a browser to prove 
+                that a person at the email address received the message. Before the site 
+                provides a contest entry, the site visitor must satisfy the following requirements for 
+                each contest registration:
+            </p>
+            <ul>
+                <li>Visit the contest offer page without being previously registered for 
+                    the same contest;</li>
+                <li>Register during a visit that begins with the referral clicking a link 
+                    provided by the referrer;</li>
+                <li>Subsequently, becoming a verified registrant in the same contest; and
+                </li>
+                <li>Performing any required action stated in the offer terms.</li>
+            </ul>
+            <p>
+                A contest may allow a verified registrant to claim email addresses before 
+                making a referral for purchase. When the site makes such a process 
+                available, the site will credit verified registrants for purchases made after 
+                the email address is claimed and before it is claimed by another registrant.
+                Two registrants may not claim responsibility for a single purchase.
+            </p>
+            <p>
+                Offers of prizes are limited. They are not binding on the site until the 
+                contest reaches a specified volume of activity. Unless stated in the offer, 
+                the limit shall be set as follows:</p>
+            <ul>
+                <li>An offer intended to increase referrals of verified registrants become 
+                    binding upon the site's owner when the number of verified registrations exceeds 
+                    10,000.</li>
+                <li>An offer intended to increase referrals of paying customers will become 
+                    binding when net operating income exceeds two times the total value of 
+                    the prizes offered.</li>
+            </ul>
+            <p>
+                A contest offer page may be disabled at any time. A contest will be 
+                terminated 3 months after the offer page is disabled to allow time for any 
+                additional purchases that verified registrants are actively referring in 
+                pursuit of a prize.
+            </p>
+            <p>
+                By participating in a contest for a prize, the registered visitor agrees to 
+                receive occasional emails from the site or the site owner. Each email will 
+                provide a link to unsubscribe. Unsubscribing from the email list removes a 
+                registered user from the contest. This notice will be repeated on the 
+                unsubscribe page.
+            </p>
+            <h2>Paying Customers</h2>
+            <p>
+                A person who pays for services only becomes a paying customer when the 
+                profile of a person who has paid money is successfully linked to the 
+                verified registrant or a claimed email address provided by a verified 
+                registrant.
+            </p>
+            <p>
+                The site will manage and maintain a list of purchases, purchase amount and 
+                estimated purchase profit. For this purpose, the site owner will upload or 
+                enter the data that is provided by payment processors. The site will 
+                compare email addresses in the purchase data to registered email addresses 
+                to identified qualified purchases. Qualified purchases will be credited 
+                to the verified registrant who either referred the customer or claimed the 
+                email address as a potential referral.
+            </p>
+            <p>The site owner will perform an update of the payments received and 
+                associated email addresses covering the life of the program before 
+                prizes linked to value provided by paying customers are awarded. The update 
+                shall be considered current and up-to-date if the data used is obtained 
+                within 24 hours of the selection, or no additional sales have been made 
+                between the time the last list was uploaded and the moment of the decision.</p>
+            <h2>Lotteries</h2>
+            <p>This site does not perform or condone the use of lotteries. A lottery is defined 
+                as a game of chance that requires entrants to pay money to improve or have a chance to win.
+                One example of a lottery would be a "gift" of "chances to win" for purchases made; 
+                even though the "chance to win" is named a gift in this hypothetical, the chance to 
+                win is only obtained through providing a purchase, and this would constitute a lottery.</p>
+            <p>This site does award chances to win in exchange for referrals of 'paying customers.' This is not 
+                a lottery. The person who receives the chance to win provides a referral only. It does not 
+                cost the registrant any money to obtain the chance, and therefore this is not a lottery but a 
+                "contest of merit."</p>
+            <p>For all contests of merit offered by this site, a person has the option of registering for 
+                a unique referral url, and that option will be made available at no cost. Please see the 
+                landing page for the 'contest of merit' registration or contact the site owner for details.</p>
+            <h2>Updates to the Policy</h2>
+            <p>
+                This policy may be updated at any time. The updated policy becomes active 
+                seven days after notice is transmitted to the registered visitors and takes 
+                immediate effect for any new visitors.
+            </p>
+            <p>
+                Offers that have not been accepted completely prior to the effective date 
+                of a new policy are revoked and reissued by the new policy terms. Actions 
+                that were taken under the previous policy will count toward consideration 
+                of the new offer under the new policy.
+            </p>
 
         </div>
     </div>
